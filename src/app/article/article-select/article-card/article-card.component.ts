cf41c9f4cec90078f83cee39164f86e607fbd554
Insomnia <915390118@qq.com>
2023-02-27 16:42:31 +0800

    article-select finished

--- src/app/article/article-select/article-card/article-card.component.ts
+++ src/app/article/article-select/article-card/article-card.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.scss']
 })
-export class ArticleCardComponent {
+export class ArticleCardComponent implements OnInit {
+  @Input('isShadow') isShadow: Boolean = false;
 
+  list = [
+    {
+      id: 0
+    },
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    }
+  ]
+
+  ngOnInit(): void {
+    console.log("isShadow: " + this.isShadow);
+  }
 }
--- src/app/article/article-select/article-select.component.ts
+++ src/app/article/article-select/article-select.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-select',
   templateUrl: './article-select.component.html',
   styleUrls: ['./article-select.component.scss']
 })
-export class ArticleSelectComponent {
+export class ArticleSelectComponent implements OnInit {
 
+  isShadow: Boolean = false;
+
+  list = [
+    {
+      id: 0
+    },
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    }
+  ]
+
+
+  ngOnInit(): void {
+    console.log(this.isShadow)
+  }
 }
