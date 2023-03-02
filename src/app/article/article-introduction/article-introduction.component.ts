919d70bcf7b283f5cd77c0224dbf9da008d45e67
Insomnia <915390118@qq.com>
2023-03-02 19:16:38 +0800

    paginator almost finished

--- src/app/article/article-introduction/article-introduction.component.ts
+++ src/app/article/article-introduction/article-introduction.component.ts
@@ -1,59 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-introduction',
   templateUrl: './article-introduction.component.html',
   styleUrls: ['./article-introduction.component.scss']
 })
-export class ArticleIntroductionComponent {
+export class ArticleIntroductionComponent implements OnInit {
   @Input('isHomePage') isHomePage: Boolean = false;
+  currentArticle: number = 0;
+  totalPage: number = 0;
+  pageArray: any = [];
+  currentPage: number = 0;
+  perArticle: number = 1;
 
   article = [
-    {
-      id: 0,
-      img: 'https://css-tricks.com/wp-content/uploads/2023/02/gradient-shadows.png',
-      tags: [
-        'box-shadow',
-        'gradients',
-        'shadow'
-      ],
-      title: 'Different Ways to Get CSS Gradient Shadows',
-      abstract: 'It\'s a question I hear asked quite often: Is it possible to create shadows from gradients instead of solid colors? There is no specific CSS property that does this (believe me, I\'ve looked) and any blog post you find about …',
-      author: {
-        name: 'Vinylon Prophet',
-        date: 'on Feb 10, 2023'
-      }
-    },
-    {
-      id: 0,
-      img: 'https://css-tricks.com/wp-content/uploads/2023/02/gradient-shadows.png',
-      tags: [
-        'box-shadow',
-        'gradients',
-        'shadow'
-      ],
-      title: 'Different Ways to Get CSS Gradient Shadows',
-      abstract: 'It\'s a question I hear asked quite often: Is it possible to create shadows from gradients instead of solid colors? There is no specific CSS property that does this (believe me, I\'ve looked) and any blog post you find about …',
-      author: {
-        name: 'Vinylon Prophet',
-        date: 'on Feb 10, 2023'
-      }
-    },
-    {
-      id: 0,
-      img: 'https://css-tricks.com/wp-content/uploads/2023/02/gradient-shadows.png',
-      tags: [
-        'box-shadow',
-        'gradients',
-        'shadow'
-      ],
-      title: 'Different Ways to Get CSS Gradient Shadows',
-      abstract: 'It\'s a question I hear asked quite often: Is it possible to create shadows from gradients instead of solid colors? There is no specific CSS property that does this (believe me, I\'ve looked) and any blog post you find about …',
-      author: {
-        name: 'Vinylon Prophet',
-        date: 'on Feb 10, 2023'
-      }
-    },
     {
       id: 0,
       img: 'https://css-tricks.com/wp-content/uploads/2023/02/gradient-shadows.png',
@@ -168,4 +128,14 @@ export class ArticleIntroductionComponent {
     {},
     {},
   ]
+
+  ngOnInit(): void {
+    this.currentArticle = this.article.length % this.perArticle;
+    this.totalPage = Math.ceil(this.article.length / this.perArticle);
+    this.pageArray = Array.from(Array(this.totalPage).keys());
+  }
+
+  updatePage(page: number) {
+    this.currentPage = page;
+  }
 }
--- src/app/component/paginator/paginator.component.ts
+++ src/app/component/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
@@ -6,46 +6,46 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/cor
   styleUrls: ['./paginator.component.scss']
 })
 export class PaginatorComponent implements OnInit, OnChanges {
-  @Input('article') article: any = [];
-  currentArticle: number = 0;
-  totalPage: number = 0;
-  pageArray: any = [];
-  currentPage: number = 0;
-  perArticle: number = 1;
+  @Input('totalPage') totalPage: number = 0;
+  @Input('pageArray') pageArray: any = [];
+  @Input('currentPage') currentPage: number = 0;
+  @Output() next = new EventEmitter();
+  @Output() previous = new EventEmitter();
+  @Output() navTo = new EventEmitter();
   prePageArray: any = [];
 
   pageElement = document.getElementsByClassName('list-number');
   activePageElement = document.getElementsByClassName('active-page');
 
   ngOnInit(): void {
-    this.currentArticle = this.article.length % this.perArticle;
-    this.totalPage = Math.ceil(this.article.length / this.perArticle);
-    this.pageArray = Array.from(Array(this.totalPage).keys());
     this.prePageArray = JSON.parse(JSON.stringify(this.pageArray));
     this.isEllipsis();
-    this.ngOnChanges(this.currentPage)
   }
 
-  ngOnChanges(changes: any): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['currentPage'] && this.prePageArray != false) {
+      this.isEllipsis();
+    }
   }
 
   nextPage() {
     this.currentPage++;
-    this.isEllipsis();
+    this.next.emit(this.currentPage);
   }
 
   prePage() {
     this.currentPage--;
-    this.isEllipsis();
+    this.previous.emit(this.currentPage);
   }
 
   toPage(event: Event) {
     this.currentPage = Number((event.target as HTMLElement).innerText) - 1;
+    this.navTo.emit(this.currentPage);
   }
 
   isEllipsis() {
     this.pageArray = JSON.parse(JSON.stringify(this.prePageArray));
-    if (this.currentPage === 0) {
+    if (this.currentPage === 0 && this.prePageArray.length >= 7) {
       this.pageArray.splice(3, this.pageArray.length - 4, '...')
     } else if (this.prePageArray.length >= 7) {
       let arrayIndex = this.pageArray.indexOf(this.currentPage);
