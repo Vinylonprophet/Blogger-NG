4e7036778698e62dde332d3bee477fa9ebbec7be
Insomnia <915390118@qq.com>
2023-03-05 16:15:08 +0800

    markdown-to-HTML

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MdToHtmlService } from '../service/md-to-html.service';
 
 @Component({
   selector: 'app-article',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent {
-
+  constructor(private MdToHtmlService: MdToHtmlService) {
+  }
+  article = this.MdToHtmlService.toHTML();
 }
